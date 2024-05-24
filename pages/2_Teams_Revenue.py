2dd17ae59d40bc335a7ae8821bfabe14306cea49
flabmax <maxence.flaba@sporteasy.net>
2024-05-24 15:18:50 +0200

    init

--- pages/2_Teams_Revenue.py
+++ pages/2_Teams_Revenue.py
@@ -9,26 +9,30 @@ st.title(':green[Teams Revenue]')
 
 with st.sidebar:
     st.write("Choose the current team type plan :")
-    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=BtJcuV&theme=horizon&opt=ctxmenu",
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=RyHRyH&theme=horizon&opt=ctxmenu",
         height=140,
         width=200)
     
     st.write("Choose the team country:")
-    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=bjAJDAZ&theme=horizon&opt=ctxmenu",
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=jnHptB&theme=horizon&opt=ctxmenu",
         height=140,
         width=200)
     
     
 st.header('1. Sales Team')
 # profiles created
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=Lhvbszb&theme=horizon&opt=ctxmenu",
+components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=XXjJWP&theme=horizon&opt=ctxmenu",
         height=500,
         width=1000)
-st.caption('*using transaction.checkout_at as the date')
+st.caption('''**Notes :** \n
+- Using transaction.checkout_at as the date \n
+- "To Date" compares amounts for the months ended in the current year with previous years over the same period''')
 
 st.header('2. MRR Team')
 # profiles created
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=Lhvbszb&theme=horizon&opt=ctxmenu",
+components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=qvjAes&theme=horizon&opt=ctxmenu",
         height=500,
         width=1000)
-st.caption('*using transaction.spread_start_at as the date')
+st.caption('''**Notes :** \n
+- Using transaction.spread_start_at as the date \n
+- "To Date" compares amounts for the months ended in the current year with previous years over the same period''')
