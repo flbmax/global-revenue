e0a8134d4a1cb7f1b5e811bcd36394da29d5a2b4
flabmax <maxence.flaba@sporteasy.net>
2024-05-24 13:01:22 +0200

    init

---
+++ pages/2_Teams_Revenue.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+
+st.set_page_config(page_title="SportEasy Revenue", page_icon='sporteasy_logo.png', layout="wide")
+
+
+st.title(':blue[Teams Revenue]')
