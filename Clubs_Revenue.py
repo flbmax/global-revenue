fb99647c3fcce632b9ccd0647b68154875b598c1
flabmax <maxence.flaba@sporteasy.net>
2024-05-24 14:40:38 +0200

    init

--- Clubs_Revenue.py
+++ Clubs_Revenue.py
@@ -9,27 +9,27 @@ st.title(':green[Clubs Revenue]')
 
 with st.sidebar:
     st.write("Choose the current club type plan :")
-    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=BtJcuV&theme=horizon&opt=ctxmenu",
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=WJWy&theme=horizon&opt=ctxmenu",
         height=140,
         width=200)
     
     st.write("Choose the club country:")
-    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=bjAJDAZ&theme=horizon&opt=ctxmenu",
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=fUKXWP&theme=horizon&opt=ctxmenu",
         height=140,
         width=200)
     
     st.write("Choose the sales:")
-    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=bjAJDAZ&theme=horizon&opt=ctxmenu",
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=JaQREm&theme=horizon&opt=ctxmenu",
         height=140,
         width=200)
     
 st.header('1. Sales Club')
 # profiles created
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=kXpw&theme=horizon&opt=ctxmenu,currsel",
+components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=kXpw&theme=horizon&opt=ctxmenu",
         height=500,
         width=1000)
-st.caption('''Notes : \n
-- using transaction.checkout_at as the date apart for the "Yearly Plan Average Basket where it is club_subscription.start_at, which means it is not possible to filter on it \n
+st.caption('''<b>Notes :</b> \n
+- Using transaction.checkout_at as the date apart for the "Yearly Plan Average Basket where it is club_subscription.start_at, which means it is not possible to filter on it \n
 - "To Date" compares amounts for the months ended in the current year with previous years over the same period''')
 
 st.header('2. MRR Club') 
