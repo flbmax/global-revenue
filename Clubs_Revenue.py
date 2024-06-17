35be80359f10d9a8c6428f47b52ca5d69fe865fc
flabmax <maxence.flaba@sporteasy.net>
2024-06-17 11:20:55 +0200

    added value

--- Clubs_Revenue.py
+++ Clubs_Revenue.py
@@ -49,13 +49,13 @@ st.caption('''**Notes :** \n
 - "To Date" compares amounts for the months ended in the current year with previous years over the same period''')
 
 
-st.header('3. Added Value/Member/Sport') 
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=JTMhpQK&theme=horizon&opt=ctxmenu",
-        height=500,
-        width=1000)
-st.caption('''**Notes :** \n
-- Calculated by divising the MRR with the number of members per month in each club
-- Mars 2022 high rate is due to one club paying 1500e with 16 members for one month \n''')
+#st.header('3. Added Value/Member/Sport') 
+#components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=JTMhpQK&theme=horizon&opt=ctxmenu",
+#        height=500,
+#        width=1000)
+#st.caption('''**Notes :** \n
+#- Calculated by divising the MRR with the number of members per month in each club
+#- Mars 2022 high rate is due to one club paying 1500e with 16 members for one month \n''')
 
 
 
